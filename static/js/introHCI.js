784f8fb8a2f56ac221e9b70f7ef86659cd04b5ca
James Rich <jcrich@ucsd.edu>
2018-01-25 13:30:41 -0800

    Changes made to lab 3 wo

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,63 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+
+		$('.jumbotron h1').text("Javascript is connected and I have just changed the text");
+		$("#testjs").text("Please wait...");
+		//selecting element extending the text
+		//$("#testjs").text("Please wait...");
+		//Adding a CSS class
+		//$("#testjs").addClass("active");
+		//adding a toggle
+		$("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+}
+
+/*
+* .append adds content
+* .html 
+* .remove, removes elements
+* .text
+*/
+function projectClick(e) { 
+
+	console.log("Project Clicked");
+	
+	//e.toggle("slow","swing",toggleToThis());
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+
+	
+	//All selected inside our containingProject class.
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+    var description = $(containingProject).find(".project-description");
+
+	
+	if (description.length == 0) {
+
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+
+    	} else {
+		
+		$(".project-description").fadeToggle("slow","swing");
+		//$(".project-description").fadeIn();
+		
+		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		//$(".project-description").fadeOut();
+		
+	}
+
 }
